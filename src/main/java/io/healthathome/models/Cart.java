f3c27304d6a137d379578929b124b3be34415387
Kedwin Johan Perez Zea <kedwpeze@suramericana.com.co>
2018-03-09 18:55:58 -0500

    Adding Id and user properties to Cart

--- src/main/java/io/healthathome/models/Cart.java
+++ src/main/java/io/healthathome/models/Cart.java
@@ -1,24 +1,39 @@
 package io.healthathome.models;
 
+import org.springframework.data.annotation.Id;
+
 import java.util.List;
 
 public class Cart {
 
+    @Id
+    private String _id;
+    private String user;
     private List<Item> items;
     private boolean state;
 
+
+    public String get_id() {
+        return _id;
+    }
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
     public boolean isState() {
         return state;
     }
-
     public void setState(boolean state) {
         this.state = state;
     }
-
     public List<Item> getItems() {
         return items;
     }
-
     public void setItems(List<Item> items) {
         this.items = items;
     }
